b68c43a98950c69107533e3fca09ceffd4497c0f
ETG1 <elijahkazungu22@gmail.com>
2024-08-22 13:25:50 +0000

    MDTsks

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- SQL script to create a table called users
+-- The table has the following fields: id, email, and name
+
+-- Create the users table if it does not already exist
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
